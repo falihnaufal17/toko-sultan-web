524cab65a03a3d64c2de1a445ad7e083fe529b25
falihnaufal17 <dudu.kotkot@gmail.com>
2020-09-03 15:13:58 +0700

    top submenu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,8 @@ import SearchBar from './SearchBar';
 import ProfilePict from '../assets/images/user-profile.png';
 
 const Navbar = props =>{
-    let authComp = (
+    let token = localStorage.getItem('token');
+    let authComp = token ? (
         <li className="nav-item">
             <div className="row align-items-center">
                 <div className="col-auto">
@@ -20,68 +21,77 @@ const Navbar = props =>{
                 </div>
             </div>
         </li>
+    ) : (
+        <>
+            <li className="nav-item">
+                <div className="valign">
+                    <div className="inner">
+                        <a className="nav-link btn-sultan btn-sultan-primary" href="/login">Masuk</a>
+                    </div>
+                </div>
+            </li>
+            <li className="nav-item">
+                <div className="valign">
+                    <div className="inner">
+                        <a className="nav-link btn-sultan btn-sultan-primary-outline" href="/register">Daftar</a>
+                    </div>
+                </div>
+            </li>
+        </>
     )
     return(
-        <nav className="navbar fixed-top navbar-expand-lg">
-            <div className="container-fluid">
-                <a className="navbar-brand d-none d-sm-none d-md-none d-lg-block" href="/">
-                    <img src={Logo} />
-                </a>
-                <a className="navbar-brand d-none d-sm-block d-md-block d-lg-none" href="/">
-                    <img src={LogoMobile} />
-                </a>
-                <div className="d-flex justify-content-between align-items-center w-100 d-sm-none">
-                    <a className="navbar-brand" href="/">
+        <>
+            <nav className="navbar fixed-top navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand d-none d-sm-none d-md-none d-lg-block" href="/">
+                        <img src={Logo} />
+                    </a>
+                    <a className="navbar-brand d-none d-sm-block d-md-block d-lg-none" href="/">
                         <img src={LogoMobile} />
                     </a>
-                    <a href="#">
-                        <img src={SearchMobile} />
-                    </a>
-                    <a href="#">
-                        <img src={BubleChatMobile} />
-                    </a>
-                    <a href="#">
-                        <img src={CartMobile} />
-                    </a>
+                    <div className="d-flex justify-content-between align-items-center w-100 d-sm-none">
+                        <a className="navbar-brand" href="/">
+                            <img src={LogoMobile} />
+                        </a>
+                        <a href="#">
+                            <img src={SearchMobile} />
+                        </a>
+                        <a href="#">
+                            <img src={BubleChatMobile} />
+                        </a>
+                        <a href="#">
+                            <img src={CartMobile} />
+                        </a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <SearchBar />
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <div className="valign">
+                                    <div className="inner">
+                                        <a href="#">
+                                            <img src={Cart}/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <div className="valign">
+                                    <div className="inner">
+                                        <div className="vline d-block"/>
+                                    </div>
+                                </div>
+                            </li>
+                            {authComp}
+                        </ul>
+                    </div>
                 </div>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <SearchBar />
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <div className="valign">
-                                <div className="inner">
-                                    <a href="#">
-                                        <img src={Cart}/>
-                                    </a>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="valign">
-                                <div className="inner">
-                                    <div className="vline d-block"/>
-                                </div>
-                            </div>
-                        </li>
-                        {/* {authComp} */}
-                        <li className="nav-item">
-                            <div className="valign">
-                                <div className="inner">
-                                    <a className="nav-link btn-sultan btn-sultan-primary" href="/login">Masuk</a>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="valign">
-                                <div className="inner">
-                                    <a className="nav-link btn-sultan btn-sultan-primary-outline" href="/register">Daftar</a>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                <div className="top-submenu d-none d-sm-none d-md-none d-lg-block">
+                    <a href="#" className="item">Komunitas</a>
+                    <a href="#" className="item">Pusat Bantuan</a>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     )
 }
 
