4fb343cf66c041c2d6b970a3145f0ace58ea12f9
falihnaufal17 <dudu.kotkot@gmail.com>
2020-09-15 13:05:11 +0700

    add dropup

--- src/components/home/floatButtonChat.js
+++ src/components/home/floatButtonChat.js
@@ -4,15 +4,23 @@ import CapaTop from '../../assets/images/capa-top.png';
 
 const FloatButtonChat = p =>{
     return(
-        <a href="#">
-            <div className="float-button">
-                <div className="float-content">
-                    <img src={FloatChatIcon} />
-                    <div className="text">Chat</div>
-                    <img src={CapaTop} />
-                </div>
+        <div className="dropup">
+            <a href="#" className="dropdown-toggle float-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                
+                    <div className="float-content">
+                        <img src={FloatChatIcon} />
+                        <div className="text">Chat</div>
+                        <img src={CapaTop} />
+                    </div>
+            </a>
+            <div className="dropdown-menu">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Separated link</a>
             </div>
-        </a>
+        </div>
     )
 }
 
